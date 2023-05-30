bb23afd6e059776c2295f236adf447cc4b9ef39b
Luthfi Izzuddin Hanif <luthfi.izzuddin1@gmail.com>
2023-05-30 22:22:04 +0700

    build: setup codebase and pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// pages and assets
+import App from './App.jsx';
+import Home from './pages/Home.jsx';
+import Promo from './pages/Promo.jsx';
+import './index.css';
+
+// router
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route exact path='/' element={<App />}>
+      <Route path='/' element={<Home />} />
+      <Route path='/promo' element={<Promo />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} fallbackElement={<h1>Loading...</h1>} />
+  </React.StrictMode>
+);
