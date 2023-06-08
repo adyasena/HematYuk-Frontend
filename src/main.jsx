94b48e0e1e019404cb9ed0e5c9c5a6abd6795460
adyasena <adyasena01@mail.ugm.ac.id>
2023-06-08 22:24:02 +0700

    fix: routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 // pages and assets
 import App from './App.jsx';
-import Homepage from './pages/Homepage.jsx';
-import Adminpage from "./pages/Adminpage.jsx";
-import Registerpage from './pages/Registerpage.jsx';
-import Loginpage from "./pages/Loginpage.jsx";
-import Promopage from './pages/Promopage.jsx';
-import './index.css';
+
 
 // router
 import {
@@ -18,20 +15,12 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route exact path='/' element={<App />}>
-      <Route path='/' element={<Homepage />} />
-      <Route path='/admin' element={<Adminpage />} />
-      <Route path='/register' element={<Registerpage />} />
-      <Route path='/login' element={<Loginpage />} />
-      <Route path='/promo' element={<Promopage />} />
-    </Route>
-  )
-);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} fallbackElement={<h1>Loading...</h1>} />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </BrowserRouter>
 );
